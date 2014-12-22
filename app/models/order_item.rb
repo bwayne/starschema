96b10eea40bb2d3a7a0d7472e5ae9f8b79c7c550
Kate Hedgpeth <katehedgpeth@gmail.com>
2014-12-22 16:14:48 -0500

    adding counter caches back to models for heroku migration

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,17 +6,17 @@ class OrderItem < ActiveRecord::Base
 	belongs_to :product_category, counter_cache: true
 	belongs_to :brand, counter_cache: true
 	
-# 	after_create :increment_Product_Customer_counter_cache
-# 	after_destroy :decrement_Product_Customer_counter_cache
-# 	
-# 	
-# 	def increment_Product_Customer_counter_cache
-# 		Product.increment_counter( "customer_count", self.product.id)
-# 	end
-# 	
-# 	def decrement_Product_Customer_counter_cache
-# 		Product.decrement_counter( "customer_count", self.product.id)
-# 	end
+	after_create :increment_Product_Customer_counter_cache
+	after_destroy :decrement_Product_Customer_counter_cache
+	
+	
+	def increment_Product_Customer_counter_cache
+		Product.increment_counter( "customer_count", self.product.id)
+	end
+	
+	def decrement_Product_Customer_counter_cache
+		Product.decrement_counter( "customer_count", self.product.id)
+	end
 	
 	
 	
