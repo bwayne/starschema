3d4f66963894c0a8c5a98c043e1d0e08db81d67d
Kate Hedgpeth <katehedgpeth@gmail.com>
2014-12-22 16:39:20 -0500

    hopefully the last migration tweak for heroku

--- db/migrate/20141219204951_add_lifecycle_to_orders.rb
+++ db/migrate/20141219204951_add_lifecycle_to_orders.rb
@@ -2,21 +2,25 @@ class AddLifecycleToOrders < ActiveRecord::Migration
 	def up
 		Customer.includes(:orders).where("total_purchases > ?", 0).each do |i|
 			orders = i.orders.all
-			first = orders[0]
-			first.lifecycle_stage_id = 2
-			first.save
-			if orders.count > 1
-				second = orders[1]
-				second.lifecycle_stage_id = 3
-				second.save
-			end
-			if orders.count > 2
-				third = orders[2]
-				third.lifecycle_stage_id = 3
-				third.save
+			if orders.count == 0
+				i.lifecycle_stage_id = 1
+			else
+				first = orders[0]				
+				first.lifecycle_stage_id = 2
+				first.save
+				if orders.count > 1
+					second = orders[1]
+					second.lifecycle_stage_id = 3
+					second.save
+				end
+				if orders.count > 2
+					third = orders[2]
+					third.lifecycle_stage_id = 3
+					third.save
+				end
+				orders.each { |i| i.lifecycle_stage_id ||= 4; i.save }
 			end
 			
-			orders.each { |i| i.lifecycle_stage_id ||= 4; i.save }
 		end
 	
 	end
