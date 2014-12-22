f32d9aa1852e19c4ef8fc49c20475cfe5452a868
Kate Hedgpeth <katehedgpeth@gmail.com>
2014-12-22 10:22:18 -0500

    Removing an unnecessary migration for heroku deployment

--- db/migrate/20141202192044_create_variation_data.rb
+++ db/migrate/20141202192044_create_variation_data.rb
@@ -1,11 +1,11 @@
 class CreateVariationData < ActiveRecord::Migration
   def change
-	Product.where("name LIKE '% in %'").each do |i|
-			separated = i.name.rpartition(" in ")
-			parent = Product.find_by name: separated[0]
-			newVariation = parent.product_variations.create
-			newVariation.name = separated[2]
-			newVariation.save
-	end
+# 	Product.where("name LIKE '% in %'").each do |i|
+# 			separated = i.name.rpartition(" in ")
+# 			parent = Product.find_by name: separated[0]
+# 			newVariation = parent.product_variations.create
+# 			newVariation.name = separated[2]
+# 			newVariation.save
+# 	end
   end
 end
